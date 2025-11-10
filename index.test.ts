7becba3675e0b4a10746443264940ec95f27fc84
mick811 <mikkelelvers@outlook.com>
2025-11-10 10:38:02 +0100

    øvelse 2. inkluderet describe, beforeEach & afterEach

--- index.test.ts
+++ index.test.ts
@@ -1,44 +1,65 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
 import { handleInput } from "./index";
 
 describe("handleInput", () => {
+    let cases: {
+        input: number | string | null | undefined
+        expected: boolean
+    }[] = []
 
-    it("should handle numbers correctly", () => {
+    // kører automatisk før hver enkel test
+    beforeEach(() => cases = []);
 
-        const cases = [
-            { input: 1, expected: true },
-            { input: 13, expected: true },
-            { input: -7, expected: false },
-            { input: 0, expected: true },
-            { input: NaN, expected: false },
-            { input: Infinity, expected: false },
-        ]
-        cases.forEach(({ input, expected }) => {
-            expect(handleInput(input), `"${input}" to return ${expected}`).toBe(expected)
+    // bruges til at nulstille mocks, close connections, etc.
+    afterEach(() => cases = []);
+
+    // grouping tests together under one "organization"
+    describe("when given numbers", () => {
+        beforeEach(() => {
+            cases = [
+                { input: 1, expected: true },
+                { input: 13, expected: true },
+                { input: -7, expected: false },
+                { input: 0, expected: true },
+                { input: NaN, expected: false },
+                { input: Infinity, expected: false },
+            ]
+        });
+
+        it("should return correct boolean values", () => {
+            cases.forEach(({ input, expected }) => {
+                expect(handleInput(input), `"${input}" to return ${expected}`).toBe(expected)
+            })
         })
     })
 
-    it("should handle strings correctly", () => {
-        
-        const cases = [
-            { input: "hello", expected: true },
-            { input: "  test ", expected: true, },
-            { input: " ", expected: false, },
-            { input: "", expected: false, }
-        ]
-        cases.forEach(({ input, expected }) => {
-            expect(handleInput(input), `"${input}" to return ${expected}`).toBe(expected)
+    describe("when given strings", () => {
+        beforeEach(() => {
+            cases = [
+                { input: "hello", expected: true },
+                { input: "  test ", expected: true, },
+                { input: " ", expected: false, },
+                { input: "", expected: false, }
+            ]
+        })
+
+        it("should return correct boolean values", () => {
+            cases.forEach(({ input, expected }) => {
+                expect(handleInput(input), `"${input}" to return ${expected}`).toBe(expected)
+            })
         })
     })
 
-    it("should handle null and undefined values", () => {
-
-        const cases = [
+    describe("when given null or undefined", () => {
+        cases = [
             { input: undefined, expected: false },
             { input: null, expected: false }
         ]
-        cases.forEach(({ input, expected }) => {
-            expect(handleInput(input), `"${input}" to return ${expected}`).toBe(expected)
+
+        it("should return false", () => {
+            cases.forEach(({ input, expected }) => {
+                expect(handleInput(input), `"${input}" to return ${expected}`).toBe(expected)
+            })
         })
     })
 });
